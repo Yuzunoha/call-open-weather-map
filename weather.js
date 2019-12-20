54b03e0df0199019ac9489c89f5db1e8e16e27a4
Yuzunoha <studysteadystandby@gmail.com>
2019-12-20 12:57:09 +0900

    取得した天気をhtmlに表示させた

--- weather.js
+++ weather.js
@@ -1,8 +1,14 @@
 const url = 'https://api.openweathermap.org/data/2.5/weather';
-
+const APPID = '4b5774e9f3d2a07b84f0f2f88e486224';
+const cityName = 'London';
 const params = new URLSearchParams();
-params.set('APPID', '4b5774e9f3d2a07b84f0f2f88e486224');
-params.set('q', 'London');
+params.set('APPID', APPID);
+params.set('q', cityName);
+const mainTextArea = document.getElementById('main-text-area');
+
+const update = (weatherDescription) => {
+  mainTextArea.innerHTML = `現在の${cityName}の天気は${weatherDescription}です`;
+}
 
 fetch(`${url}?${params}`)
   .then((response) => {
@@ -12,8 +18,6 @@ fetch(`${url}?${params}`)
     const responseObj = JSON.parse(responseText);
     const weatherDescription = responseObj.weather[0].description;
     console.log(weatherDescription);
+
+    update(weatherDescription);
   });
-
-const mainTextArea = document.getElementById('main-text-area');
-mainTextArea.innerHTML = 'JSから書きました';
-
